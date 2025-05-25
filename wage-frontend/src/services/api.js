f1e2550c4edd66edb57e8dfc3caa508e40afbbeb
LUCKY SHARMA <luckysharma2708@gmail.com>
2025-05-26 04:57:48 +0530

    changed url

--- wage-frontend/src/services/api.js
+++ wage-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://wage-calculator-fjir.onrender.com",
+  baseURL: "https://wage-calculator-fjir.onrender.com/api/employees",
 });
 
 export default API;
